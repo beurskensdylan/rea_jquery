e15fb523abd8ddaf3a17e1bf20683a45f5db8770
Dylan Beurskens <info@dylanbeurskens.com>
2022-07-15 16:15:41 +0200

    first commit

---
+++ les1_jquery/opdracht1/js/script.js
@@ -0,0 +1,8 @@
+alert('TEST 1 2 3');
+
+$(Document).ready(function() {
+    $('#h2').hide();
+    $('#container > p:nth-child(4)').hide();
+    $('#container > .new:first-child').hide();
+    $('#container > .new:last-child').hide();
+})
