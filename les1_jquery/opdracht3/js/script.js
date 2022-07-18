1d324c588a4edf07a22127d86d5525896536d6b9
Dylan Beurskens <info@dylanbeurskens.com>
2022-07-18 10:15:25 +0200

    Les 1 opdracht 3 af

--- les1_jquery/opdracht3/js/script.js
+++ les1_jquery/opdracht3/js/script.js
@@ -1,4 +1,4 @@
-$(Document).ready(function() {
+$(document).ready(function() {
     $('section:first-of-type .row-1:first-of-type').on('click', function() {
         $('section:last-of-type .row-1').hide();
     });
@@ -8,10 +8,28 @@ $(Document).ready(function() {
     $('section:first-of-type .row-2').on('click', function() {
         $('section:last-of-type .row-2').toggle();
     });
-    $('section:first-of-type .row-3:first-of-type').on('click', function() {
-        $('section:last-of-type .row-3').slideUp(1000);
+    $('section:first-of-type button:nth-child(4)').on('click', function() {
+        $('section:last-of-type .row-3').slideUp();
     });
-    $('section:first-of-type .row-3:nth-child(2)').on('click', function() {
-        $('section:last-of-type .row-3').slideDown(1000);
+    $('section:first-of-type button:nth-child(5)').on('click', function() {
+        $('section:last-of-type .row-3').slideDown();
+    });
+    $('section:first-of-type button:nth-child(6)').on('click', function() {
+        $('section:last-of-type .row-4').slideToggle();
+    });
+    $('section:first-of-type button:nth-child(7)').on('click', function() {
+        $('section:last-of-type .row-5').fadeOut();
+    });
+    $('section:first-of-type button:nth-child(8)').on('click', function() {
+        $('section:last-of-type .row-5').fadeIn();
+    });
+    $('section:first-of-type button:nth-child(9)').on('click', function() {
+        $('section:last-of-type .row-6').fadeToggle();
+    });
+    $('section:first-of-type button:nth-child(10)').on('click', function() {
+        $('section:last-of-type .row-7').fadeTo(500, 0.5);
+    });
+    $('section:first-of-type button:nth-child(11)').on('click', function() {
+        $('section:last-of-type .row-7').fadeTo(500, 1.0);
     });
 })
